a9ad940a622424b2b17fb373f55977e84434e479
Tien Nguyen <tien.ng1020@gmail.com>
2019-07-28 20:48:29 -0500

    updated

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,6 +7,8 @@ module.exports = function(app) {
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
-
+  app.get("/github", function(req, res) {
+    res.status(301).redirect("https://github.com/tien1020/FriendFinder")
+  });
   
 }
